0a1d7c408fe18c0326d44a256c68e7fd8ad795fd
hikibi-muma <2623573402@qq.com>
2020-04-01 19:24:54 +0800

    基础页面

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import {HashRouter,Route} from 'react-router-dom'
+import React, { Component } from 'react';
+import {HashRouter,Route} from 'react-router-dom';
+import Admin from './pages/Admin';
+import Login from './pages/Login';
+import User from './pages/User'
 
-function App() {
-  return (
-    <div className="App">
+// const Login = loadalbe(()=>import('./pages/Login'))
+// const Admin = loadalbe(()=>import('./pages/Admin'))
+class App extends Component{
+  render(){
+      return(
      <HashRouter>
-       <div>
-         <h1>项目启动</h1>
-       </div>
-     </HashRouter>
-    </div>
+        <Route path='/login' component={Login}></Route>
+        <Route path='/admin' render ={()=>{
+            return(
+              <Admin>
+                <Route path='/admin/user' component={User}></Route>
+
+              </Admin>
+            )
+          }}></Route>
+        </HashRouter>
   );
+}
 }
 
 export default App;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React,{ Component} from 'react';
+class Login extends Component {
+ 
+    state={ }
+    render(){
+         return(
+            <div>登录页面页面</div>
+         );
+     }
+}
+
+
+export default Login;
